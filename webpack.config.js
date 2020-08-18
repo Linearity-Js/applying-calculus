95278d6521324e701a740ca7aa77cff2104ec3f1
Rodrigo R. Rubio Haro <chavo0022009@gmail.com>
2020-08-18 09:54:02 -0500

    webpack: file loader was added

--- webpack.config.js
+++ webpack.config.js
@@ -18,17 +18,53 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
                 test: /\.js?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
                 }
-            }, {
-                test: /\.svelte?$/,
-                exclude: /node_modules/,
+            },
+            {
+                test: /\.svelte$/,
                 use: {
                     loader: 'svelte-loader'
                 }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
             }
         ]
     },
