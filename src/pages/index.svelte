72689ff5a771968c78b5a45398758f68d42fdae5
Rodrigo R. Rubio Haro <chavo0022009@gmail.com>
2020-08-18 11:28:57 -0500

    routify and npm-run-all were added

---
+++ src/pages/index.svelte
@@ -0,0 +1,68 @@
+<script>
+  import Header from "../components/Header.svelte";
+  import Logo from "../components/Logo.svelte";
+  import Main from "../components/Main.svelte";
+
+  import { faBook } from "@fortawesome/free-solid-svg-icons/faBook";
+  import Fa from "svelte-fa";
+  let icon = faBook;
+
+  import { isDark } from "../stores/stores";
+  import Button from "sveltestrap/src/Button.svelte";
+
+  isDark.subscribe((value) => {
+    value
+      ? document.body.classList.add("dark")
+      : document.body.classList.remove("dark");
+  });
+
+  function handleDarkMode() {
+    $isDark = !$isDark;
+  }
+</script>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap");
+  :global(:root) {
+    --light-theme-body-background: #ffff;
+    --dark-theme-body-background: #1c1e21;
+  }
+
+  :global(body) {
+    background-color: var(--light-theme-body-background);
+    transition: 0.3s ease;
+  }
+
+  :global(body.dark) {
+    background-color: var(--dark-theme-body-background);
+    color: white;
+  }
+
+  :global(Button) {
+    font-family: "Montserrat", sans-serif;
+    /* text-transform: uppercase; */
+    padding: 1rem 3.5rem !important;
+    line-height: 1.5 !important;
+    color: white !important;
+    font-size: 2rem !important;
+    border-radius: 1rem !important;
+    outline: none !important;
+    border: none !important;
+  }
+  :global(Button):focus {
+    box-shadow: none !important;
+  }
+</style>
+
+<Header />
+<Main>
+
+  <Logo />
+  <Button color="warning">
+    <Fa {icon} size="sm" />
+    Learn
+  </Button>
+  <!-- <Button on:click={handleDarkMode}/> -->
+</Main>
+<!-- 
+<button on:click={handleDarkMode}>dark mode</button> -->
