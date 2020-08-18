a67eb1d187e14f1e28f51799899e597a6aa1ee7d
Rodrigo R. Rubio Haro <chavo0022009@gmail.com>
2020-08-17 23:38:00 -0500

    basic structure was added

---
+++ src/components/Button.svelte
@@ -0,0 +1,3 @@
+<div class="Button">
+    button
+</div>
---
+++ src/containers/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Header from "../components/Header.svelte";
+  import Logo from "../components/Logo.svelte";
+  import Button from "../components/Button.svelte";
+  import Main from "../components/Main.svelte";
+</script>
+
+<Header />
+<Main>
+  <Logo />
+  <Button />
+</Main>
