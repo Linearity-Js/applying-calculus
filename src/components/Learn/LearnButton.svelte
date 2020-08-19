74fbe56725f2a221d163f60c66414ed437af069e
Rodrigo R. Rubio Haro <chavo0022009@gmail.com>
2020-08-19 06:22:17 -0500

    Learn Button was added

---
+++ src/components/Learn/LearnButton.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Button from "sveltestrap/src/Button.svelte";
+
+  import { faBook } from "@fortawesome/free-solid-svg-icons/faBook";
+  import Fa from "svelte-fa";
+  let icon = faBook;
+
+  export let text;
+</script>
+
+<Button color="warning">
+  <Fa {icon} size="sm" />
+  {text}
+</Button>
