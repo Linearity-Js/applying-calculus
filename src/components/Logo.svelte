ea609a9b110e4f5bb2170b0cd6a2366202d3ffa6
Rodrigo R. Rubio Haro <chavo0022009@gmail.com>
2020-08-19 06:21:26 -0500

    Logo was fixed: full image

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,49 +1,17 @@
 <script>
   import App from "../containers/App.svelte";
-  import img from "../img/logo.png";
+  import img from "../img/applying-calculus.png";
   import { Col, Container, Row } from "sveltestrap";
-  import { fade } from "svelte/transition";
-  let visible = false;
-  import { onMount } from "svelte";
-  onMount(async () => {
-    if (!visible) {
-      setTimeout(1000);
-      visible = true;
-    }
-  });
+  let visible = true;
 </script>
 
-<style>
-  @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap");
-  .Logo-title {
-    font-family: "Montserrat", sans-serif;
-    text-transform: uppercase;
-    font-size: 5rem;
-  }
-
-  .flex-center-vertically {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    height: 400px;
-  }
-</style>
-
 {#if visible}
-  <div class="Logo" transition:fade>
-
+  <div class="Logo">
     <Container>
       <Row>
         <Col>
           <div class="Logo-image">
-            <img src={img} alt="por definir" />
-          </div>
-        </Col>
-        <Col>
-          <div class="Logo-title flex-center-vertically">
-            Applying
-            <br />
-            Calculus
+            <img class="img-fluid" src={img} alt="por definir" />
           </div>
         </Col>
       </Row>
